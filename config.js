b3268ddbbd0b55e8058d90b7d88f7829b048b578
Dima-CS2 <zacwinston19@gmail.com>
2026-02-13 23:30:32 -0600

    Update music streaming URL in config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "res.cloudinary.com/dn1vjxcvi/video/upload/v1771045420/Stephen_Sanchez_-_Until_I_Found_You__mp3.pm_gylq2t.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dn1vjxcvi/video/upload/v1771045420/Stephen_Sanchez_-_Until_I_Found_You__mp3.pm_gylq2t.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
